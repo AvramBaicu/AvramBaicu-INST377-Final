51d0ec58ad3c7a4cfa324e8243c9f160e96389f6
Avram Baicu <37336756+AvramBaicu@users.noreply.github.com>
2023-04-25 14:13:03 -0400

    Update 1.13
    Added map onto website.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function initMap(){
-    const carto = L.map("map").setView([38.98, -76.93],13);
+    const carto = L.map("map").setView([38.98, -76.93],10);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(carto);
 return carto;
+ function initGraph() {
+  const chart = new CanvasJS.Chart("chartContainer", {
+  });
+
+  chart.render();
+}
+
 
   }    const carto = initMap();
+
+  async function mainEvent(){
+    console.log('start');
+    initMap();
+    initGraph();
+  }
+  document.addEventListener('DOMContentLoaded', async() => mainEvent())
