df9907edf6b0003355faccd26b9c3c1eebc5e577
Avram Baicu <37336756+AvramBaicu@users.noreply.github.com>
2023-04-25 22:45:31 -0400

    Update 1.15
    Graphics and about page is done.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AvramBaicu-INST377-Final
  Working with API's website.
+Link to get it up and running: https://avrambaicu.github.io/AvramBaicu-INST377-Final/
--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-function initMap(){
-    const carto = L.map("map").setView([38.98, -76.93],10);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+function initMap() {
+  const carto = L.map("map").setView([41.850033, -97.6500523], 4);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(carto);
-return carto;
- function initGraph() {
-  const chart = new CanvasJS.Chart("chartContainer", {
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(carto);
+  return carto;
+}
+// function initGraph() {
+//   const chart = new CanvasJS.Chart("chartContainer", {});
+
+//   chart.render();
+// }
+
+async function mainEvent() {
+  console.log("start");
+  const mapbtn = document.querySelector("#mapbtn");
+  const graphbtn = document.querySelector("#graphbtn");
+  const mapdisplay = document.querySelector("#map");
+  const graphdisplay = document.querySelector("#chartContainer");
+
+  graphbtn.addEventListener("click", () => {
+    graphdisplay.classList.toggle("hidden");
+    mapdisplay.classList.toggle("hidden");
+    console.log("graph changed");
   });
 
-  chart.render();
+  mapbtn.addEventListener("click", () => {
+    mapdisplay.classList.toggle("hidden");
+    graphdisplay.classList.toggle("hidden");
+    console.log("map changed");
+  });
+   
+  // initGraph();
+  const ctx = document.getElementById("chart");
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+    
+  });
+  // const instance = M.Tabs.init(document.querySelector(".tabs"));
+
+const map = initMap();
+
 }
-
-
-  }    const carto = initMap();
-
-  async function mainEvent(){
-    console.log('start');
-    initMap();
-    initGraph();
-  }
-  document.addEventListener('DOMContentLoaded', async() => mainEvent())
+document.addEventListener("DOMContentLoaded", async () => mainEvent());
